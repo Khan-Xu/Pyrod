0c3b767ac7750916e9b0ec1ba6c93b491d4cdfd4
Khan-Xu <65407325+Khan-Xu@users.noreply.github.com>
2021-04-15 16:25:40 +0800

    Add files via upload

---
+++ read/base.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+#----------LIBRARYS-----------
+
+# exceptions library
+from _exception import (The_Parameter_Exception, The_Vars_Check_Exception)
+# python processing library  
+import numpy as np
+# private library 
+from text import _check_data
+
+#----------CONSTANTS----------
+
